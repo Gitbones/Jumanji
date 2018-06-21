1c21069debc5915d9c040ee9341ceeeb60eeab0d
Gabriel Sperb Stoffel <gsperbstoffel@gmail.com>
2018-06-20 21:08:31 -0300

    Corrigindo e implementando funções

--- game.py
+++ game.py
@@ -6,26 +6,28 @@ from evento import *
 
 class Game(object):
 
-    def __init__(self, tabuleiro, jogador, limite_jogadas=12):
+    def __init__(self, tabuleiro, jogador, limite_jogadas=15):
         self.__tabuleiro = tabuleiro
         self.__jogador = jogador
         self.__limite_jogadas = limite_jogadas
+        self.eventos = [Macaco(), Gorila(), Vento(), Jacare(), Rinoceronte(), Nada(), Enchente()]
 
     @property
     def jogador(self):
         return self.__jogador
 
     def executa_evento(self):
-        eventos = [Macaco(), Gorila(), Vento(), Jacare(), Rinoceronte(), Nada(), Enchente()]
-        eventos[randint(0, len(eventos) - 1)].executa(self)
+        self.eventos[randint(0, len(self.eventos) - 1)].executa(self)
 
     def jogar_dados(self):
-        self.__jogador.andar_frente(int(randint(0, 6)))
+        self.__jogador.andar_frente(int(randint(1, 6)))
         self.executa_evento()
         self.diminuir_jogadas(1)
 
     def diminuir_jogadas(self, value):
         self.__limite_jogadas -= value
+        if(self.__limite_jogadas < 0):
+            self.__limite_jogadas = 0
 
     def perder(self):
         return self.__limite_jogadas <= 0
@@ -35,6 +37,13 @@ class Game(object):
 
 if __name__ == '__main__':
 
-    g = Game(Tabuleiro(), Jogador(), 10)
-    g.jogar_dados()
-
+    g = Game(Tabuleiro(), Jogador())
+    while(not g.ganhar() and not g.perder()):
+        g.jogar_dados()
+        print(g.jogador.posicao)
+        if(g.ganhar()):
+            print("Ganhou!")
+            break
+        elif(g.perder()):
+            print("Perdeu!")
+            break
